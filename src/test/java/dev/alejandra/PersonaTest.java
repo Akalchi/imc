b1dbc6dfdf8cd44aac9b272ff4b63793cdf7411e
Akalchi <alejandrafdez1699@gmail.com>
2025-02-10 14:14:24 +0100

    "REFACTOR TEST - implemented beforeEach"

--- src/test/java/dev/alejandra/PersonaTest.java
+++ src/test/java/dev/alejandra/PersonaTest.java
@@ -1,20 +1,31 @@
 package dev.alejandra;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 public class PersonaTest {
+    
+    private Persona persona;
+    private double kg = 72.6;
+    private double meters = 1.75;
+
+    @BeforeEach
+    void beforeEach() {
+
+        persona = new Persona(kg, meters);
+    }
+
+    
 
     @Test
     void testReturnKg() {
 
-        double kg = 72.6; 
-        double meters = 1.67;
-
-        Persona persona = new Persona(kg, meters);
-
         assertThat(persona.getKg(), is(kg));
-     
+    }
+    @Test
+    void testReturnMetres() {
+
     }
 
 }
