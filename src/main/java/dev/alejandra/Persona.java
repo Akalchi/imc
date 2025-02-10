0391c671df860008e4e8a6660bb1c57c6f0c0c1b
Akalchi <alejandrafdez1699@gmail.com>
2025-02-10 15:10:39 +0100

    "FEAT [determineIMCCategory] implemented to fix failing tests"

--- src/main/java/dev/alejandra/Persona.java
+++ src/main/java/dev/alejandra/Persona.java
@@ -20,6 +20,22 @@ public class Persona {
         return Math.round(this.kg / (Math.pow(this.meters, 2)) * 100) / 100.0;
     }
 
-   
+    public String determineIMCCategory() {
+        double imc = calculateIMC();
+
+        if (imc < 18.5) {
+            return imc < 16 ? "Delgadez severa" : (imc < 17 ? "Delgadez moderada" : "Delgadez leve");
+        } else if (imc < 25) {
+            return "Peso normal";
+        } else if (imc < 30) {
+            return "Sobrepeso";
+        } else if (imc < 35) {
+            return "Obesidad leve";
+        } else if (imc < 40) {
+            return "Obesidad moderada";
+        } else {
+            return "Obesidad mórbida";
+        }
+    }
 
 }
