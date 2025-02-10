f19da7eb5b9b76b29e4d3e42bb9db35ae7a35a31
Akalchi <alejandrafdez1699@gmail.com>
2025-02-10 14:42:17 +0100

    "FEAT [calculateIMC] implemented to fix failing tests"

--- src/main/java/dev/alejandra/Persona.java
+++ src/main/java/dev/alejandra/Persona.java
@@ -3,6 +3,7 @@ package dev.alejandra;
 public class Persona {
     private double kg;
     private double meters;
+
     public Persona(double kg, double meters) {
         this.kg = kg;
         this.meters = meters;
@@ -13,6 +14,10 @@ public class Persona {
 
     public double getMeters(){
         return this.meters;
+    }
+
+    public double calculateIMC() {
+        return Math.round(this.kg / (Math.pow(this.meters, 2)) * 100) / 100.0;
     }
 
 }
